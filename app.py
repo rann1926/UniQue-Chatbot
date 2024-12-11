dd7d9c13379a4b6125dbaa6807b555f269652264
rann1926 <167492773+rann1926@users.noreply.github.com>
2024-12-11 19:43:48 +0800

    update

--- app.py
+++ app.py
@@ -21,35 +21,25 @@ model = SentenceTransformer('all-Mpnet-base-v2')
 patterns = [pattern for intent in intents['intents'] for pattern in intent['patterns']]
 pattern_embeddings = model.encode(patterns, convert_to_tensor=True)
 
+# Prediction function using Sentence Transformers
 def predict(user_input):
 
     if profanity.contains_profanity(user_input):
         return "Please be respectful. Let's keep our conversation friendly. "
-    
-    if re.match(r'^[^a-zA-Z]*$', user_input) or len(set(user_input)) <= 3:
-        return "I’m having trouble understanding that. Can you give me a bit more detail about what you're asking?"
 
-    # Fuzzy matching check
-    best_match_score_fuzzy = 0
-    best_match_idx_fuzzy = -1
-    for i, pattern in enumerate(patterns):
-        score = fuzz.ratio(user_input.lower(), pattern.lower())
-        if score > best_match_score_fuzzy:
-            best_match_score_fuzzy = score
-            best_match_idx_fuzzy = i
-
-    # Sentence transformer cosine similarity check
     user_embedding = model.encode(user_input, convert_to_tensor=True)
     cosine_scores = util.pytorch_cos_sim(user_embedding, pattern_embeddings)[0]
-    best_match_idx_cosine = np.argmax(cosine_scores)
-    best_match_score_cosine = cosine_scores[best_match_idx_cosine]
+    best_match_idx = np.argmax(cosine_scores)
+    best_match_score = cosine_scores[best_match_idx]
 
-    # Combine both fuzzy matching and cosine similarity results
-    if best_match_score_fuzzy > 70 or best_match_score_cosine > 0.70: 
-        best_match_idx = best_match_idx_fuzzy if best_match_score_fuzzy > best_match_score_cosine else best_match_idx_cosine
+    if best_match_score > 0.7:  # Adjust threshold as needed
+        # Find the corresponding intent
         for intent in intents['intents']:
             if patterns[best_match_idx] in intent['patterns']:
                 return random.choice(intent['responses'])
+
+    if re.match(r'^[^a-zA-Z]*$', user_input) or len(set(user_input)) <= 3:
+        return "I’m having trouble understanding that. Can you give me a bit more detail about what you're asking?"
 
     return """My current version does not have the specific information for your query. For further assistance, you can contact the specific concerned offices:\n\n<b>Registrar's Office:</b>\nTelephone: (043) 416-0350 local 214\nEmail: registrar.nasugbu@g.batstate-u.edu.ph
 \n<b>Testing and Admission Office:</b>\nTelephone: 416-0350 local 216\nEmail: tao.nasugbu@g.batstate-u.edu.ph\nOfficial Facebook Page: https://fb.com/TestingAndAdmissionOffice"""
